00905a33b81c834b4be522c71aaafeb016bec85c
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 15:51:25 -0400

    xuyisen__diff__id_473

--- setup.py
+++ setup.py
@@ -64,7 +64,22 @@ def parse_requirements(extras_require_map):
             else:
                 raise ValueError("Invalid version format")
 
-            if (major, minor) >= (2, 7):
+            if (major, minor) >= (2, 10):
+                _install_requires.pop(_install_requires.index(xformers_version))
+                _install_requires.append("xformers>=0.0.34")
+            elif (major, minor) >= (2, 9):
+                _install_requires.pop(_install_requires.index(xformers_version))
+                if patch == 0:
+                    _install_requires.append("xformers==0.0.33")
+                else:
+                    _install_requires.append("xformers==0.0.33.post2")
+            elif (major, minor) >= (2, 8):
+                _install_requires.pop(_install_requires.index(xformers_version))
+                if patch == 0:
+                    _install_requires.append("xformers==0.0.32.post1")
+                else:
+                    _install_requires.append("xformers==0.0.32.post2")
+            elif (major, minor) >= (2, 7):
                 _install_requires.pop(_install_requires.index(xformers_version))
                 if patch == 0:
                     _install_requires.append("xformers==0.0.30")
