976e6a337e5d677b0ea0a395b244409c278c7352
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 15:50:04 -0400

    xuyisen__diff__id_449

--- tests/e2e/multigpu/test_fsdp2.py
+++ tests/e2e/multigpu/test_fsdp2.py
@@ -10,6 +10,7 @@ from accelerate.test_utils import execute_subprocess_async
 from transformers.testing_utils import get_torch_dist_unique_port
 
 from axolotl.utils.dict import DictDefault
+from ..utils import require_torch_2_6_0
 
 AXOLOTL_ROOT = Path(__file__).parent.parent.parent.parent
 
